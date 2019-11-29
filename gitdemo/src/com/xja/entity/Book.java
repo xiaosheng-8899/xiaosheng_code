eb52f194c1661c4bc86dd3f1b5f0f83ee221d706
xiaosheng <671846152@qq.com>
2019-11-29 16:38:05 +0800

    sss

---
+++ gitdemo/src/com/xja/entity/Book.java
@@ -0,0 +1,24 @@
+package com.xja.entity;
+/** 
+* @author 作者 :lixiaosheng 
+* @date 创建时间：2019年11月29日 下午4:37:19 
+* @version 1.0 
+*/
+public class Book {
+	private Integer bookId;
+	private String bookName;
+	public Integer getBookId() {
+		return bookId;
+	}
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	
+	
+}
